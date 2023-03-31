dc24a1ab1843d5baddd8b89e8dd5384496305865
Tahseen <tahseenislam@hotmail.com>
2023-04-01 00:14:03 +1000

    refactored code

--- server.js
+++ server.js
@@ -10,41 +10,38 @@ app.use(
     extended: true,
   })
 );
-
 app.use(cors());
 app.options('*', cors());
 
-app.get('/', (req, resp) => {
-  resp
-    .status(200)
-    .json({ message: 'Hi welcome to the TMDB API for tahseenio' });
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hi welcome to the TMDB API for tahseenio' });
 });
 
-app.get('/popular', async (req, resp) => {
+app.get('/popular', async (req, res) => {
   const POPULAR_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=en-US&page=1`;
-  await fetchData(POPULAR_URL, resp);
+  await fetchData(POPULAR_URL, res);
 });
 
-app.get('/trending', async (req, resp) => {
+app.get('/trending', async (req, res) => {
   const TRENDING_URL = `https://api.themoviedb.org/3/trending/all/day?api_key=${apikey}`;
-  await fetchData(TRENDING_URL, resp);
+  await fetchData(TRENDING_URL, res);
 });
 
-app.get('/toprated', async (req, resp) => {
+app.get('/toprated', async (req, res) => {
   const TOP_RATED_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apikey}&language=en-US&page=1`;
-  await fetchData(TOP_RATED_URL, resp);
+  await fetchData(TOP_RATED_URL, res);
 });
 
-app.get('/searched', async (req, resp) => {
+app.get('/searched', async (req, res) => {
   const movie = req.query.movie;
   const SEARCHED_URL = `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&language=en-US&query=${movie}&page=1&include_adult=false`;
-  await fetchData(SEARCHED_URL, resp);
+  await fetchData(SEARCHED_URL, res);
 });
 
-app.get('/banner', async (req, resp) => {
+app.get('/banner', async (req, res) => {
   const id = req.query.id;
   const MOVIE_BANNER_URL = `https://api.themoviedb.org/3/movie/${id}?api_key=${apikey}&language=en-US`;
-  await fetchData(MOVIE_BANNER_URL, resp);
+  await fetchData(MOVIE_BANNER_URL, res);
 });
 
 const fetchData = async (url, response) => {
